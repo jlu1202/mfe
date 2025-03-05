eae068608117193bb361f30497f9915d2089f86d
James Lu <james.lu@aspentech.com>
2025-03-05 12:55:17 -0500

    Changes to account for namespace in the css styling.

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-// import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
-// import { mount } from 'marketing/MarketingApp';
-
-// console.log(mount);
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
